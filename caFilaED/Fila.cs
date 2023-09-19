a43c6f9f823b178ce9dc860bf35c7550ac03c349
duh-h <cadugon1000@yahoo.com>
2023-09-19 17:15:25 -0300

    Adicionar arquivos de projeto.

---
+++ caFilaED/Fila.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caFilaED
+{
+    internal class Fila
+    {
+        public Noh Head { get; set; }
+
+
+        public Fila()
+        {
+            this.Head = new Noh(0);
+            this.Head.anterior = null;
+            this.Head.proximo = null;
+        }
+
+        public void addFim(int info)
+        {
+            Noh novoNoh = new Noh(info);
+
+            if (this.Head.anterior == null)
+            {
+                this.Head.anterior = novoNoh;
+                this.Head.proximo = novoNoh;
+            }
+            else
+            {
+                novoNoh.anterior = this.Head.proximo;
+                this.Head.proximo.proximo = novoNoh;
+                this.Head.proximo = novoNoh;
+            }
+
+        }
+        public void removeInicio()
+        {
+
+
+
+            Head.anterior.proximo.anterior = null;
+            Head.anterior = Head.anterior.proximo;
+        }
+
+
+
+        public void Imprimirlista()
+        {
+            Noh atual = Head.anterior;
+
+            while (atual != null)
+            {
+                Console.WriteLine(atual.info);
+                atual = atual.proximo;
+            }
+
+            Console.WriteLine();
+        }
+        public int Remove()
+        {
+            return Head.proximo.info;
+        }
+
+        public void InsirirLugar(int info)
+        {
+            Noh atual = Head.anterior;
+            Noh novo = new Noh(info);
+
+            while (atual.info != info)
+            {
+                atual = atual.proximo;
+            }
+            novo.proximo = atual.proximo;
+            atual.proximo.anterior= novo;
+            atual.proximo = novo;
+            novo.anterior = atual;
+            novo.info = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+
+       
+      
+
+    }
+}
+
---
+++ caFilaED/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caFilaED
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fila minhaFila = new Fila();
+
+            string controle;
+
+            Console.WriteLine("[1] Insira um inteiro na Fila");
+            Console.WriteLine("[2] Remova um inteiro da Fila");
+            Console.WriteLine("[3] Insira um inteiro apos um outro a ser escolhido");
+            Console.WriteLine("[4] Imprima a Fila");
+            Console.WriteLine("[5] SAIR");
+
+            controle = Console.ReadLine();
+
+            while (controle != "5")
+            {
+                switch (controle)
+                {
+                    case "1":
+                        Console.WriteLine("Insira um inteiro:");
+                        int valor = Convert.ToInt32(Console.ReadLine());
+                        minhaFila.addFim(valor);
+                        break;
+                    case "2":
+                        minhaFila.removeInicio();
+                        break;
+                    case "3":
+                        Console.WriteLine("Escolha o numero:");
+                        int insira = Convert.ToInt32(Console.ReadLine());
+                        minhaFila.InsirirLugar(insira);
+
+                        break;
+                    case "4":
+                        Console.WriteLine("Fila:");
+                        minhaFila.Imprimirlista();
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+
+                Console.WriteLine("[1] Insira um inteiro na Fila");
+                Console.WriteLine("[2] Remova um inteiro da Fila");
+                Console.WriteLine("[3] Insira um inteiro apos um outro a ser escolhido");
+                Console.WriteLine("[4] Imprima a Fila");
+                Console.WriteLine("[5] SAIR");
+
+                controle = Console.ReadLine();
+            }
+
+        }
+    }
+}
+        
+    
+
